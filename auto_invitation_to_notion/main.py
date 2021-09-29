61d721ad5da20e39191d759b2202af80b398d0d2
Martin Hwang <dhhwang89@gmail.com>
2021-09-30 01:51:13 +0900

    선영님 리뷰 반영 및 동규님 리뷰 일부 반영

--- auto_invitation_to_notion/main.py
+++ auto_invitation_to_notion/main.py
@@ -9,41 +9,34 @@ from notion.client import NotionClient
 LUBYCON_PRIVATE_PAGE = '4594ead1-b975-451d-a578-2ffeea6aa452'
 LUBYCON_MENTOR_PAGE = '720e43a5-f572-440a-8e58-847b72359b16'
 LUBYCON_HUB_PAGE = 'd3ebe34b-34af-4b2f-b984-ae1e91cff7f3'
+LUBYCON_EMAIL = "lubycon@gmail.com"
 
 
 def get_lubycon_admin_uid(client: NotionClient) -> str:
-    return client.get_email_uid().get('lubycon@gmail.com')
+    return client.get_email_uid().get(LUBYCON_EMAIL)
 
 
 def get_workspace_id(client: NotionClient, admin_uid: str) -> str:
     response = client.post('getSpaces', {})
-
-    if not response.ok:
-        raise Exception(handle_request_error(response=response))
+    response.raise_for_status()
 
     workspace_id = list(response.json()[admin_uid]['space'].keys())[-1]
     return workspace_id
 
 
-def handle_request_error(response: requests.Response) -> str:
-    return f"request fail!. status code: {response.status_code}, detail: {response.text}"
-
-
 def get_uid_pageids_assigned_users(client: NotionClient, workspace_id: str) -> Dict:
     response = client.post("getSubscriptionData", {
                            "spaceId": workspace_id, "version": "v2"})
-
-    if not response.ok:
-        raise Exception(handle_request_error(response=response))
+    response.raise_for_status()
 
     users_info = response.json()["users"]
 
-    answer = {}
+    uid_pageids_info = {}
     for user_info in users_info:
-        answer[user_info["userId"]] = {
+        uid_pageids_info[user_info["userId"]] = {
             "guest_page_ids": user_info.get("guestPageIds") or []}
 
-    return answer
+    return uid_pageids_info
 
 
 def detect_authority(user_info: Dict) -> Dict:
@@ -81,30 +74,29 @@ def get_email_name_assigned_users(client: NotionClient, user_uid_pageids: List[s
                 "version": -1} for user_uid in list(user_uid_pageids.keys())]
 
     response = client.post("syncRecordValues", {"requests": payload})
-    if not response.ok:
-        raise Exception(handle_request_error(response=response))
+    response.raise_for_status()
 
     notion_user = response.json()["recordMap"]["notion_user"]
 
-    ans = {}
+    user_info = {}
     for user_uid in notion_user:
         v = notion_user[user_uid]["value"]
-        ans[user_uid] = {"email": v["email"], "name": v["name"]}
-    return ans
+        user_info[user_uid] = {"email": v["email"], "name": v["name"]}
+    return user_info
 
 
 def change_pk_to_email(user_info: Dict):
-    ans = {}
+    changed_user_info = {}
 
     for user_uid in user_info.keys():
         email = user_info[user_uid]["email"]
         name = user_info[user_uid]["name"]
         authority = user_info[user_uid]["authority"]
         guest_page_ids = user_info[user_uid]["guest_page_ids"]
-        ans[email] = {"uid": user_uid, "name": name,
-                      "authority": authority, "guest_page_ids": guest_page_ids}
+        changed_user_info[email] = {"uid": user_uid, "name": name,
+                                    "authority": authority, "guest_page_ids": guest_page_ids}
 
-    return ans
+    return changed_user_info
 
 
 def get_notion_users_info(client: NotionClient):
